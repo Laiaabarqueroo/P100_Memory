2b0412f99caa27207318152ca30deaafd5b5504a
Laia Barquero <barquero.b.laia@gmail.com>
2024-04-11 01:53:50 +0200

    Cards created
    Added all the cards
    All the cards turn

---
+++ js/p100-memoria.js
@@ -0,0 +1,34 @@
+var ampladaCarta, alcadaCarta;
+var separacioH = 20,
+    separacioV = 20;
+var nFiles = 3,
+    nColumnes = 6;
+var jocCartes = [];
+
+$(function() {
+    ampladaCarta = $(".carta").width();
+    alcadaCarta = $(".carta").height();
+    // Mida del tauler
+    $("#tauler").css({
+        "width": (nColumnes * (ampladaCarta + separacioH) + separacioH) + "px",
+        "height": (nFiles * (alcadaCarta + separacioV) + separacioV) + "px"
+    });
+
+    var contador = 0;
+    for (var f = 1; f <= nFiles; f++) {
+        for (var c = 1; c <= nColumnes; c++) {
+            contador++;
+            var carta = $('<div class="carta" id="f' + f + 'c' + c + '"><div class="cara darrera"></div><div class="cara davant"></div></div>');
+            carta.css({
+                "left": ((c - 1) * (ampladaCarta + separacioH) + separacioH) + "px",
+                "top": ((f - 1) * (alcadaCarta + separacioV) + separacioV) + "px"
+            });
+            carta.find(".davant").addClass(jocCartes[contador % jocCartes.length]);
+            $("#tauler").append(carta);
+        }
+    }
+
+    $(".carta").on("click", function() {
+        $(this).toggleClass("carta-girada");
+    });
+});
