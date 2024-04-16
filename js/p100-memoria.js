0abecd1637fa85181202f3ddf1f37c34dde13f3c
AinaBarnet <147982964+AinaBarnet@users.noreply.github.com>
2024-04-16 09:12:57 +0200

    counter

--- js/p100-memoria.js
+++ js/p100-memoria.js
@@ -1,11 +1,11 @@
-var ampladaCarta, alcadaCarta;
-var separacioH = 20,
+let ampladaCarta, alcadaCarta;
+const separacioH = 20,
     separacioV = 20;
-var nFiles = 3,
+const nFiles = 3,
     nColumnes = 6;
-var jocCartes = [];
+const jocCartes = [];
 
-$(function() {
+$(function () {
     ampladaCarta = $(".carta").width();
     alcadaCarta = $(".carta").height();
     // Mida del tauler
@@ -14,21 +14,21 @@ $(function() {
         "height": (nFiles * (alcadaCarta + separacioV) + separacioV) + "px"
     });
 
-    var contador = 0;
-    for (var f = 1; f <= nFiles; f++) {
-        for (var c = 1; c <= nColumnes; c++) {
-            contador++;
-            var carta = $('<div class="carta" id="f' + f + 'c' + c + '"><div class="cara darrera"></div><div class="cara davant"></div></div>');
+    let contador = 1;
+    for (let f = 1; f <= nFiles; f++) {
+        for (let c = 1; c <= nColumnes; c++) {
+            const carta = $('<div class="carta" id="f' + f + 'c' + c + '"><div class="cara darrera"></div><div class="cara davant"></div></div>');
             carta.css({
                 "left": ((c - 1) * (ampladaCarta + separacioH) + separacioH) + "px",
                 "top": ((f - 1) * (alcadaCarta + separacioV) + separacioV) + "px"
             });
             carta.find(".davant").addClass(jocCartes[contador % jocCartes.length]);
             $("#tauler").append(carta);
+            contador++;
         }
     }
 
-    $(".carta").on("click", function() {
+    $(".carta").on("click", function () {
         $(this).toggleClass("carta-girada");
     });
 });
